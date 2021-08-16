d7e7d98df12e917a7ee70f6e8e27dce085656004
Bebeto Alves <bebeto.inbox@gmail.com>
2021-08-16 12:48:02 -0300

    Adiciona o CORS para possibilitar conexões externas.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ require('dotenv').config()
 
 const express = require('express')
 const logger = require('morgan')
+const cors = require('cors')
 const db = require('./models')
 
 const app = express()
 const PORT = process.env.PORT
 
 app.use(logger('dev'))
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
